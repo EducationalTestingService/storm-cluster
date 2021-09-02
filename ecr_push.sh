4f6cb198f444b2d7d9b7e09042d12f96f2197df1
Vyacheslav Andreyev <sandreyev@ets.org>
2021-09-02 14:55:42 -0400

    refactored ecr-related scripts

--- ecr_push.sh
+++ ecr_push.sh
@@ -3,11 +3,13 @@
 set -euo pipefail
 image_name=nlparch-storm-cluster
 aws_region=us-east-1
+registry=435708183536.dkr.ecr.us-east-1.amazonaws.com
+# registry=docker.io/etslabs
 
 # Tag your podman image:
 podman tag \
        $image_name:latest \
-       435708183536.dkr.ecr.us-east-1.amazonaws.com/$image_name:latest
+       $registry/$image_name:latest
 
 # Push your podman image to the registry:
-podman push 435708183536.dkr.ecr.us-east-1.amazonaws.com/$image_name:latest
+podman push $registry/$image_name:latest
