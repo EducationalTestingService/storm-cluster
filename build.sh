0afa1b6acb7fe995b848b66f77e27686c021fc22
Vyacheslav Andreyev <sandreyev@ets.org>
2023-11-09 10:24:29 -0500

    updated the version of Mambaforge

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 image_name=storm-cluster
+if command -v podman > /dev/null; then
+    docker=podman
+else
+    docker=docker
+fi
 
-podman build --no-cache -t $image_name .
+$docker build --no-cache -t $image_name .
--- registry_push.sh
+++ registry_push.sh
@@ -6,25 +6,31 @@ if [[ ${USERNAME:-} ]]; then
 else
     docker_login=${USER:?}
 fi
+if command -v podman > /dev/null; then
+    docker=podman
+else
+    docker=docker
+fi
 
 image_name=storm-cluster
 registry=docker.io/etslabs
 tag=${tag:-latest}
 
 if [[ $tag != 'latest' ]]; then
-    if podman image exists "localhost/$image_name:$tag"; then
+    if (( $( $docker image ls "$image_name:$tag" | wc -l ) == 2 )); then
         : ok
     else
-        podman tag "localhost/$image_name:latest" "localhost/$image_name:$tag"
+        $docker tag "$image_name:latest" "$image_name:$tag"
     fi
 fi
 
-# Tag your podman image:
-podman tag \
-       "localhost/$image_name:$tag" \
-       "$registry/$image_name:$tag"
+# Tag your docker image:
+$docker tag \
+        "$image_name:$tag" \
+        "$registry/$image_name:$tag"
 
 echo "INFO: Using '$docker_login' to push new image to docker.io ..." >&2
 
-# Push your podman image to the registry:
-podman push "$registry/$image_name:$tag" "--creds=$docker_login"
+# Push your docker image to the registry:
+$docker login --username "$docker_login"
+$docker push "$registry/$image_name:$tag"
